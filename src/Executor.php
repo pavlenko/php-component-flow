467ca67ba4cd075bd5325428574256c20040f43b
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2018-12-20 08:13:21 +0200

    process each node source separatelly

--- src/Executor.php
+++ src/Executor.php
@@ -21,20 +21,17 @@ final class Executor
      * @param array       $options
      * @param string|null $nodeID
      */
-    public function execute(array &$options = [], string $nodeID = null)
+    public function execute(array &$options = [], string $nodeID = null): void
     {
         foreach ($this->flow->getNodes() as $node) {
-            if (null === $nodeID || $node->getID() == $nodeID) {
-                $sources = $this->flow->getSourcesOf($node);
-                $results = [];
-
-                foreach ($sources as $source) {
-                    foreach ($source->results($options) as $result) {
-                        $results[] = $result;
+            if (null === $nodeID || $node->getID() === $nodeID) {
+                if (count($sources = $this->flow->getSourcesOf($node))) {
+                    foreach ($sources as $source) {
+                        $node->process($source->results($options), $options);
                     }
+                } else {
+                    $node->process([], $options);
                 }
-
-                $node->process($results, $options);
             }
         }
     }
