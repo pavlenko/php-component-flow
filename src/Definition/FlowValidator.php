a8974bd5607cc6e6c9e0539233f54b3dfa892841
Eugene Pavlenko <eugene.p@doubledmarketing.com>
2019-10-10 16:26:52 +0300

    add minimal flow validator code

---
+++ src/Definition/FlowValidator.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace PE\Component\Flow\Definition;
+
+final class FlowValidator implements FlowValidatorInterface
+{
+    /**
+     * @var NodeValidatorInterface[]
+     */
+    private $validators = [];
+
+    /**
+     * @param NodeValidatorInterface[] $validators
+     */
+    public function __construct(array $validators)
+    {
+        $this->validators = [];
+
+        foreach ($validators as $validator) {
+            if (!($validator instanceof NodeValidatorInterface)) {
+                throw new \UnexpectedValueException(sprintf(
+                    'Validator must be instance of %s, but got %s',
+                    NodeValidatorInterface::class,
+                    is_object($validator) ? get_class($validator) : gettype($validator)
+                ));
+            }
+
+            $this->validators[] = $validator;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validate(FlowInterface $flow): array
+    {
+        $result = [];
+
+        foreach ($flow->getNodes() as $node) {
+            $errors = [];
+
+            foreach ($this->validators as $validator) {
+                if ($validator->supports($node) && count($messages = $validator->validate($flow, $node))) {
+                    array_push($errors, ...$messages);
+                }
+            }
+
+            if ($errors) {
+                $result[] = [$node, $errors];
+            }
+        }
+
+        return $result;
+    }
+}
--- src/Definition/NodeValidatorInterface.php
+++ src/Definition/NodeValidatorInterface.php
@@ -5,6 +5,13 @@ namespace PE\Component\Flow\Definition;
 interface NodeValidatorInterface
 {
     /**
+     * @param NodeInterface $node
+     *
+     * @return bool
+     */
+    public function supports(NodeInterface $node): bool;
+
+    /**
      * @param FlowInterface $flow
      * @param NodeInterface $node
      *
